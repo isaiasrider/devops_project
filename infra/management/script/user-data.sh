74119d2423f7ef3f57f22765670e1ccbe41533a0
isaiasrider <isaiasdelima1@gmail.com>
2021-02-27 17:42:09 -0300

    install docker and git

--- infra/management/script/user-data.sh
+++ infra/management/script/user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-yum -y install java-1.8.0-openjdk
+yum -y install java-1.8.0-openjdk docker git
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum -y install jenkins
