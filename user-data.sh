3332ee9001b0cf0e4dd665793a02a4f352fdead5
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 13:09:06 -0300

    try to resolve docker login directly inside user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -6,4 +6,5 @@ yum install docker -y
 dockerd &
 ls -l /var/run/
 chmod 777 /var/run/docker.sock
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 622021885326.dkr.ecr.us-east-2.amazonaws.com
 docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${container_version}
