165d30f2457cf1852b52583c9ace32ef4236c145
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 01:52:46 -0300

    fix version typo to test

--- application_deployment.tf
+++ application_deployment.tf
@@ -1,5 +1,5 @@
 module "deployment" {
-  source = "github.com/isaiasrider/terraform-modules//modules/services/webserver_cluster?ref=devops-v1.0.1"
+  source = "github.com/isaiasrider/terraform-modules//modules/services/webserver_cluster?ref=devops-1.0.1"
   cluster_name = "web-app-${BUILD_NUMBER}"
   container_version = "${GIT_COMMIT}"
   enable_autoscaling = false
