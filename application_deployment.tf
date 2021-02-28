6f4179022155823b4a8610259c9e35f894e3acae
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 03:25:05 -0300

    escapte $

--- application_deployment.tf
+++ application_deployment.tf
@@ -10,10 +10,10 @@ module "deployment" {
   container_version = var.container_version
   user_data = <<EOF
 #!/bin/bash
-container_version=${container_version}
+container_version=\${container_version}
 yum update -y
 yum install docker
-docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${container_version}
+docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:\${container_version}
 
 EOF
 
