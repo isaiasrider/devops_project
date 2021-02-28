570c83e9cf86cf37e3145ff6e3476121c0319393
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 15:14:31 -0300

    add iam role to login into ecr and download container's images

--- application_deployment.tf
+++ application_deployment.tf
@@ -2,13 +2,14 @@ provider "aws" {
   region = "us-east-2"
 }
 module "deployment" {
-  source = "github.com/isaiasrider/terraform-modules//modules/services/webserver_cluster?ref=devops-v1.0.4"
+  source = "github.com/isaiasrider/terraform-modules//modules/services/webserver_cluster?ref=devops-v1.0.5"
   cluster_name = var.build
   enable_autoscaling = false
   max_size = 2
   min_size = 1
   container_version = var.container_version
   user_data = data.template_file.user-data.rendered
+  instance_profile = data.aws_iam_instance_profile.profile.id
 
 
 
@@ -22,3 +23,9 @@ data template_file "user-data" {
 
   }
 }
+
+data aws_iam_instance_profile "profile" {
+  name = "AdminAccess"
+}
+
+
