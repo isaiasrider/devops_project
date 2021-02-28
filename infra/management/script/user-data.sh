4eca610811004fda8bb75cdd0309af52be430f18
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 01:24:17 -0300

    install terraform to te jenkins server

--- infra/management/script/user-data.sh
+++ infra/management/script/user-data.sh
@@ -4,5 +4,8 @@
 yum -y install java-1.8.0-openjdk docker git
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip
+unzip terraform_0.12.28_linux_amd64.zip
+cp -p terraform /usr/bin/
 yum -y install jenkins
 service jenkins start
