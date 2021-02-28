a344fd08ac7e6585b1a13f4f2f12a0651891b38b
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 11:14:10 -0300

    agora vai!

--- user-data.sh
+++ user-data.sh
@@ -2,5 +2,5 @@
 
 container_version=${container_version}
 yum update -y
-yum install docker
+yum install docker -y
 docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${container_version}
