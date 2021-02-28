fe63d23e7848e10ff4d76129c582462e84e932a5
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 12:51:25 -0300

    debug docker execution

--- user-data.sh
+++ user-data.sh
@@ -4,5 +4,6 @@ container_version=${container_version}
 yum update -y
 yum install docker -y
 dockerd &
+ls -l /var/run/
 chmod 777 /var/run/docker.sock
 docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${container_version}
