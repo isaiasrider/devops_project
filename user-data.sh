a6803c57237ff0a08bc084c58c216f4663a0674f
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 12:38:01 -0300

    let asg capable to run containers

--- user-data.sh
+++ user-data.sh
@@ -3,4 +3,6 @@
 container_version=${container_version}
 yum update -y
 yum install docker -y
+dockerd &
+chmod 777 /var/run/docker.sock
 docker run -d -p80:3000 622021885326.dkr.ecr.us-east-2.amazonaws.com/devops:${container_version}
