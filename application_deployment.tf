d8314a77b66be54b00427323b5e26f5bb5276cf0
isaiasrider <isaiasdelima1@gmail.com>
2021-02-28 02:09:05 -0300

    update module to support container_version variables

--- application_deployment.tf
+++ application_deployment.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 module "deployment" {
-  source = "github.com/isaiasrider/terraform-modules//modules/services/webserver_cluster?ref=devops-v1.0.2"
+  source = "github.com/isaiasrider/terraform-modules//modules/services/webserver_cluster?ref=devops-v1.0.3"
   cluster_name = "web-app-${var.container_version}"
   enable_autoscaling = false
   max_size = 2
